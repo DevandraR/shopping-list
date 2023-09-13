4c635fdf36991d692acb0d901a221f0eba887ad6
Devandra <devandra271203@gmail.com>
2023-09-13 15:36:06 +0700

    Tutorial 2 Finishing

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\dvndr\SEM 3\PBP\shopping_list\env
+set VIRTUAL_ENV=D:\UI\SEM 3\PBP\shopping_list\env
 
 if not defined PROMPT set PROMPT=$P$G
 
